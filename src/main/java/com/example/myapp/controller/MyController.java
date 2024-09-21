af80a6bf1147bff8b338782d4874442b567d80dc
unknown <myaws1991@gmail..com>
2024-09-21 23:56:06 +0530

    My commit-1

---
+++ src/main/java/com/example/myapp/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.example.myapp.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MyController {
+
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
